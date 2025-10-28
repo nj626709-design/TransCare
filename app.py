422f5d98ddc916c3a7d32ad4533d06b0d5af6cf7
Pratiksha Jadhav <nj626709@gmail.com>
2025-10-28 07:22:34 +0530

    Save current work before pulling

--- app.py
+++ app.py
@@ -120,4 +120,5 @@ Message: {qr.message}
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="127.0.0.1", port=5000, debug=True)
+
