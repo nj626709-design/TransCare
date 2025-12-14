76f89c9c0818f15a45a25beec60db9463a3fcd40
Pratiksha Jadhav <nj626709@gmail.com>
2025-12-14 16:36:25 +0530

    Fix Flask import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_mail import Mail, Message
 from datetime import datetime
 
